3def1f9c9d7d73c8ae6ac000f0f13c0f622f8a98
unknown <mariamaydin59@gmail.com>
2024-12-12 16:03:40 +0800

    backend is now working, will proceed to frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,7 @@
-//require("dotenv").config(); // This should load the .env file
-
-//console.log(process.env);
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
